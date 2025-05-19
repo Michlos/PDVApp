ea18bcd0e3d8f6b2dcc9d2d952c36a209a415747
Michlos <michlos@gmail.com>
2025-05-18 22:57:45 -0300

    chore: Implements Services and IServices

---
+++ PDVApp.Api/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using PDVApp.Api.DTOs;
+
+namespace PDVApp.Api.Services;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<IEnumerable<CategoryDTO>> GetCategoriesProducts();
+    Task<CategoryDTO> GetCategoryById(int id);
+    Task AddCategory(CategoryDTO categoryDto);
+    Task UpdateCategory(CategoryDTO categoryDto);
+    Task RemoveCategory(int id);
+
+}
---
+++ PDVApp.Api/Services/ProductService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+
+using PDVApp.Api.DTOs;
+using PDVApp.Api.Models;
+using PDVApp.Api.Repository;
+
+namespace PDVApp.Api.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ProductDTO>> GetProducts()
+    {
+        var productEntity = await _productRepository.GetAll();
+        return _mapper.Map<IEnumerable<ProductDTO>>(productEntity);
+    }
+    public async Task<ProductDTO> GetProductById(int id)
+    {
+        var productEntity = await _productRepository.GetById(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+    
+    public async Task AddProduct(ProductDTO productDto)
+    {
+        var productEntity = _mapper.Map<Product>(productDto);
+        await _productRepository.Create(productEntity);
+        productDto.Id = productEntity.Id;
+    }
+    public async Task UpdateProduct(ProductDTO productDto)
+    {
+        var productEntity = _mapper.Map<Product>(productDto);
+        await _productRepository.Update(productEntity);
+    }
+
+    public async Task RemoveProduct(int id)
+    {
+        var productEntity = _productRepository.GetById(id);
+        if (productEntity == null)
+            throw new Exception("Service: Product not found");
+        await _productRepository.Delete(id);
+    }
+
+}
