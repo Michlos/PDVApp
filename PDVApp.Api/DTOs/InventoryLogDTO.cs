26887ff99d937becb0b5a09847aefd2e3cfc2869
Michlos <michlos@gmail.com>
2025-05-19 22:43:43 -0300

    feature: Add InventoryLog and InventoryLogDTO

---
+++ PDVApp.Api/DTOs/InventoryLogDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace PDVApp.Api.DTOs;
+
+public class InventoryLogDTO
+{
+
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int PreviousQuantity { get; set; }
+    public int QuantityChanged { get; set; }
+    public int CurrentQuantity { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string? OriginType { get; set; }
+    public int? OriginId { get; set; }
+    public string? Action { get; set; }
+    public int? UserId { get; set; }
+
+}
---
+++ PDVApp.Api/Models/InventoryLog.cs
@@ -0,0 +1,29 @@
+﻿namespace PDVApp.Api.Models;
+
+public class InventoryLog
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int PreviousQuantity { get; set; }
+    public int QuantityChanged { get; set; }
+    public int CurrentQuantity { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    public OriginType OriginType { get; set; } // e.g., Sale, Purchase, Adjustment, Transfer
+    public int? OriginId { get; set; } //e.g. SaleId, PurschaseId, AdjustmentId, TransferId
+    public Action Action { get; set; } // e.g., Added, Removed
+    public int? UserId { get; set; } // ID of the user who made the change
+}
+
+public enum OriginType
+{
+    Sale,
+    Purchase,
+    Adjustment,
+    Transfer
+}
+
+public enum Action
+{
+    Added,
+    Removed
+}
