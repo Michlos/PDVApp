a3daabfdb1b679bf0266fd1edb7983ee6a1fea39
Michlos <michlos@gmail.com>
2025-05-19 22:19:12 -0300

    feature: add second migration AlterProductFields

---
+++ PDVApp.Api/Migrations/20250520011634_AterProductEntity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PDVApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AterProductEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Barcode",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "Products",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinQuantity",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Barcode",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "MinQuantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Products");
+        }
+    }
+}
