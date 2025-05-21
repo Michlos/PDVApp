9491eceb2a67cfc8b4cc8df4aceddee3c5c2f8f8
Michlos <michlos@gmail.com>
2025-05-20 23:04:32 -0300

    feature: added InventoryLogController for Gets only

---
+++ PDVApp.Api/Controllers/InventoryLogController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using PDVApp.Api.DTOs;
+using PDVApp.Api.Services;
+
+namespace PDVApp.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class InventoryLogController : ControllerBase
+{
+    private readonly IInventoryLogService _inventoryLogService;
+    public InventoryLogController(IInventoryLogService inventoryLogService)
+    {
+        _inventoryLogService = inventoryLogService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<InventoryLogDTO>>> Get()
+    {
+        var inventoryLogsDto = await _inventoryLogService.GetInventoryLogs();
+        if (inventoryLogsDto is null)
+            return NotFound("No inventory logs found.");
+        return Ok(inventoryLogsDto);
+    }
+
+    [HttpGet("product")]
+    public async Task<ActionResult<IEnumerable<InventoryLogDTO>>> GetInventoryByProduct(int productId)
+    {
+        var inventoryLogsDto = await _inventoryLogService.GetInventoryByProduct(productId);
+        if (inventoryLogsDto is null)
+            return NotFound("No inventory logs found.");
+        return Ok(inventoryLogsDto);
+    }
+
+}
